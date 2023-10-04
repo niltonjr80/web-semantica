0156ad40dc4ecb5f8c50725c194c96dbeb69be6e
Nilton Junior <14946185+niltonjr80@users.noreply.github.com>
2023-10-04 19:16:54 +0000

    Final Update Assignment 3 - SPARQL

--- rdf-simpsons/src/main/java/br/com/niltonjr80/websemantica/SparqlQueryEngine.java
+++ rdf-simpsons/src/main/java/br/com/niltonjr80/websemantica/SparqlQueryEngine.java
@@ -1,5 +1,4 @@
 package br.com.niltonjr80.websemantica;
-
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -12,44 +11,65 @@ public class SparqlQueryEngine {
     public static void main(String[] args) {
 
         if (args.length != 1) {
-            System.out.println("Uso: ./gradlew runSparqlQueryEngine --args=\"exercise_x\" (onde x é o numero do exercício)");
+            System.out.println("Usage: ./gradlew runSparqlQueryEngine --args=\"exercise_x\" (where x is the exercise number)");
             return;
         }
 
-        // Nome do arquivo de entrada passado como argumento
+        // Name of the input file passed as an argument
         String inputFileName = args[0];
 
-        // Caminho para o arquivo de consulta SPARQL
+        // Path to the SPARQL query file
         String sparqlQueryFile = "src/main/resources/input/" + inputFileName + ".rq";
 
-        // Caminho para o arquivo RDF dos Simpsons
+        // Path to the RDF file of the Simpsons
         String rdfFile = "src/main/resources/input/simpsons.ttl";
 
-        // Caminho para o arquivo de saída
+        // Path to the output file
         String outputFile = "src/main/resources/output/output_" + inputFileName + ".txt";
 
-        // Criando um fluxo de saída para o arquivo
+        // Creating an output stream for the file
         try (OutputStream os = new FileOutputStream(outputFile)) {
             PrintStream output = new PrintStream(os);
 
-            // Redirecionnando a saída padrão para o arquivo e o console
+            // Redirecting the standard output to both the file and the console
             System.setOut(output);
 
-            // Carregando o gráfico RDF dos Simpsons
+            // Loading the RDF graph of the Simpsons
             Model model = ModelFactory.createDefaultModel();
-            //model.setNsPrefix("sim", "http://www.ifi.uio.no/IN3060/simpsons#");
             FileManager.get().readModel(model, rdfFile, "TURTLE");
 
-            // Carregando a consulta SPARQL do arquivo
+            // Loading the SPARQL query from the file
             String sparqlQuery = FileManager.get().readWholeFileAsUTF8(sparqlQueryFile);
 
-            // Executando a consulta SPARQL
-            Query query = QueryFactory.create(sparqlQuery);
-            try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
-                ResultSet results = qexec.execSelect();
+            // Checking the type of query (SELECT, ASK, or CONSTRUCT)
+            if (sparqlQuery.contains("SELECT")) {
+                // SELECT query
+                Query query = QueryFactory.create(sparqlQuery);
+                try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+                    ResultSet results = qexec.execSelect();
 
-                // Exibindo os resultados em forma de tabela
-                ResultSetFormatter.out(System.out, results, query);               
+                    // Displaying the results as a table
+                    ResultSetFormatter.out(System.out, results, query);
+                }
+            } else if (sparqlQuery.contains("ASK")) {
+                // ASK query
+                Query query = QueryFactory.create(sparqlQuery);
+                try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+                    boolean result = qexec.execAsk();
+                    System.out.println("Result of ASK query: " + result);
+                }
+            } else if (sparqlQuery.contains("CONSTRUCT")) {
+                // CONSTRUCT query
+                Query query = QueryFactory.create(sparqlQuery);
+                try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+                    // Creating a Model to store the result triples
+                    Model resultModel = qexec.execConstruct();
+
+                    // Output the result model in the desired serialization format (e.g., Turtle)
+                    resultModel.write(output, "TURTLE");
+                }
+            } else {
+                System.out.println("Unsupported query type.");
             }
         } catch (Exception e) {
             e.printStackTrace();
